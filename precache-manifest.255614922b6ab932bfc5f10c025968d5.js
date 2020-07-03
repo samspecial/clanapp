dd6d9b3cc390afcd08837b87f8f3f17be60208e7
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2020-07-03 17:39:19 +0100

    Updates

---
+++ precache-manifest.255614922b6ab932bfc5f10c025968d5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4fb7c1ebca60f5b45219a8a0372377",
+    "url": "/clanapp/index.html"
+  },
+  {
+    "revision": "527b1489dabef563d0ec",
+    "url": "/clanapp/static/css/main.232b1586.chunk.css"
+  },
+  {
+    "revision": "4fe99bd27febdd32b3fd",
+    "url": "/clanapp/static/js/2.cf1c3176.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clanapp/static/js/2.cf1c3176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "527b1489dabef563d0ec",
+    "url": "/clanapp/static/js/main.1899a1b5.chunk.js"
+  },
+  {
+    "revision": "a17724f03a845c6143c8",
+    "url": "/clanapp/static/js/runtime-main.669034e2.js"
+  },
+  {
+    "revision": "eee7284a0ed8b7ca1f37c90c7088f95f",
+    "url": "/clanapp/static/media/active.eee7284a.svg"
+  },
+  {
+    "revision": "13599628456b8c6a6a6a04221e1b6107",
+    "url": "/clanapp/static/media/apply.13599628.svg"
+  },
+  {
+    "revision": "f083135010cc9adc5daf107c8953164e",
+    "url": "/clanapp/static/media/categorize.f0831350.svg"
+  },
+  {
+    "revision": "2081e10884d3e8c5bde0521a52d8f5af",
+    "url": "/clanapp/static/media/contact.2081e108.svg"
+  },
+  {
+    "revision": "d83527403c4a767d97a4af02bed83fc1",
+    "url": "/clanapp/static/media/help.d8352740.svg"
+  },
+  {
+    "revision": "a692699889cf816b3e2e94aed6a6d76a",
+    "url": "/clanapp/static/media/mail.a6926998.svg"
+  },
+  {
+    "revision": "fbd5da5c3d2e17983e564293077ca01a",
+    "url": "/clanapp/static/media/notification.fbd5da5c.svg"
+  },
+  {
+    "revision": "39d530354818d7f8715ef914c2801afc",
+    "url": "/clanapp/static/media/pencil.39d53035.svg"
+  },
+  {
+    "revision": "09919846c7faafea931ff410cb506221",
+    "url": "/clanapp/static/media/transaction.09919846.svg"
+  }
+]);
